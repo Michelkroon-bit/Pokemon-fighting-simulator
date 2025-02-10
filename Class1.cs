d9c732e3f82e8289da79c52c61179a80c0799ac7
Michelkroon-bit <99076850@mydavinci.nl>
2025-02-10 16:51:21 +0100

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemons
+{
+    public class Pokemon
+    {
+        public string naam { get; set; }
+        public string strength { get; set; }
+        public string weakness  { get; set; }
+
+        public int HP { get; set; }
+
+        public int Atk { get; set; }
+
+        public Pokemon(string naam ,string strength , string weakness , int HP , int Atk) 
+        {
+            this.naam = naam;
+            this.strength = strength;
+            this.weakness = weakness;
+            this.HP = HP;
+            this.Atk = Atk;
+        }
+    }
+}
